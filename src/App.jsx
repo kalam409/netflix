629c5e016ef1405e798ff20dc6153044f6d20bd8
kalam409 <kalamislam409@gmail.com>
2023-01-24 23:44:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
+function App() {
+  return (
+    <div className="App">
+      <h3><AccountCircleTwoToneIcon /></h3>
+      <h1 className='text-sky-400/100 text-2xl font-bold text-center'>Netflix Site !!</h1>
+    </div>
+  )
+}
+
+export default App
